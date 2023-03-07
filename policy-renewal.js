88e51f7956d0121359a493afbfb1a7647d83a31f
Raj Jain <rajjain@gb3crem0q0pvg.dyn.oneds.com>
2023-03-07 15:59:41 +0000

    can click the policy renew section

--- policy-renewal.js
+++ policy-renewal.js
@@ -5,8 +5,13 @@ let driver = new Builder().forBrowser("chrome").build();
 
 async function policyRenewal() {
   await webDriver.webDriver(driver);
-  await driver.get("https://iquw-uat3.sequel.com/origin/Policy/20");
-  await driver.wait(until.elementLocated(By.css("span")), 10000);
+  await driver.get("https://iquw-uat3.sequel.com/origin/Policy/24");
+  await driver.wait(
+    until.elementLocated(
+      By.className("dropdown__label dropdown__label--policyscreengreenheader")
+    ),
+    10000
+  );
   var actions = driver.findElement(
     By.className("dropdown__label dropdown__label--policyscreengreenheader")
   );
@@ -17,6 +22,20 @@ async function policyRenewal() {
     )
   );
   renew.click();
+  await driver.wait(
+    until.elementLocated(
+      By.xpath(
+        "/html/body/origin-layout/div/div[3]/origin-policy-details/origin-policy-details-directive/split-screen-container/div/article/policy-screen/div/div[4]/div/div[3]/button"
+      )
+    ),
+    10000
+  );
+  var renewSection = driver.findElement(
+    By.xpath(
+      "/html/body/origin-layout/div/div[3]/origin-policy-details/origin-policy-details-directive/split-screen-container/div/article/policy-screen/div/div[4]/div/div[3]/button"
+    )
+  );
+  renewSection.click();
 }
 
 policyRenewal();
